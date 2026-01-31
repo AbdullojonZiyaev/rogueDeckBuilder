6ebe9310463bfd2fbf728034a499233564958af0
tera <dn8157@rit.edu>
2026-01-31 19:55:22 +0400

    added card's constructors and getters

--- objects/card.py
+++ objects/card.py
@@ -7,20 +7,24 @@ class Card:
     WP = winning points(ПО)
     ability = свойство (что делает?)
     """
-    def __init__ (self, name, power, cost, WP, ability):
+    def __init__ (self, card_index, name, power, cost, WP, ability):
+        self.card_index = card_index
         self.name = name
         self.power = power
         self.cost = cost
         self.WP = WP
         self.ability = ability
+    
+    def getCard (self):
+        return self.card_index
 
-    def GetPower (self):
+    def getPower (self):
         return self.power
     
-    def GetName (self):
+    def getName (self):
         return self.name
     
-    def GetCost (self):
+    def getCost (self):
         return self.cost
     
     def getWP (self):
